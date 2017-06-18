1833e7d370a616c3c20c7b7bb44175f01beac15a
whearn <wchearn00@gmail.com>
2017-06-18 16:39:39 -0400

    Completed Lab

--- server/index.js
+++ server/index.js
@@ -1,7 +1,88 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+var clientPath = path.join(__dirname, '..', 'client');
 
 var server = http.createServer(function(req, res) {
+    var urlData = url.parse(req.url, true);
+    if (urlData.pathname === '/' && req.method === 'GET') {
+        //requests homepage
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        fs.createReadStream(path.join(clientPath, 'index.html')).pipe(res);
+    } else if (urlData.pathname === '/api/chirps') {
+        if (req.method === 'GET') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            // fs.readFile(path.join(__dirname, 'data.json'), function(err, contents) {
+            //     if (err) {
+            //         console.log(err);
+            //     } else {
+            //         var dataContent = JSON.parse(contents);
+            //         console.log(dataContent);
+            //     }
+            // });
+            fs.createReadStream(path.join(__dirname, 'data.json')).pipe(res);
+        } else if (req.method === 'POST') {
+            fs.readFile(path.join(__dirname, 'data.json'), 'utf8', function(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    //turns into an array
+                    var chirps = JSON.parse(data);
+                    var incomingData = '';
+                    req.on('data', function(chunk) {
+                        incomingData += chunk;
+                    });
+                    //all pieces of data have been received
+                    req.on('end', function() {
+                        var newChirp = JSON.parse(incomingData);
+                        chirps.push(newChirp);
 
+                        // Now we need to convert instructors array back to JSON
+                        // and write the JSON string back to data.json
+                        fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(instructors), function(err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                // everything is ok and finished
+                                res.writeHead(201, { 'Content-Type': 'application/json' });
+                                res.end('{}');
+                            }
+                        });
+                    });
+                }
+            });
+        } else {
+
+        }
+    } else if (req.method === 'GET') {
+        //need to try to read requested file from client folder
+        var filePath = path.join(clientPath, urlData.pathname);
+        var readStream = fs.createReadStream(filePath);
+        readStream.on('error', function(e) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('File Not Found');
+        });
+        var extension = path.extname(filePath);
+        var contentType;
+
+        switch (extension) {
+            case '.html':
+                contentType = 'text/html';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            default:
+                contentType = 'text/plain';
+        }
+
+        res.writeHead(200, { 'Content-Type': contentType});
+        readStream.pipe(res);
+    }
 });
 server.listen(3000);
